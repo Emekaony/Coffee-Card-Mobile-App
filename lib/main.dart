64119bd2327191c2fb2fb40600cb53b49cc2b969
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-03-03 12:42:29 -0800

    Changed from center to container

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text("Hello world")));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Coffee Card"),
+          backgroundColor: Colors.brown[700],
+        ),
+        body: Container(
+          color: Colors.orange,
+          width: 200,
+          height: 200,
+          margin: EdgeInsets.fromLTRB(10, 30, 0, 0),
+          child: Text("Hello Emeka"),
+        ),
+      ),
+    );
+  }
 }
