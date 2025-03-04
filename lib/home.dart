fb25effc3e688b000d1f5b971aef62b6aabce987
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-03-04 12:25:11 -0800

    Moved home widget into its own file

---
+++ lib/home.dart
@@ -0,0 +1,24 @@
+import "package:flutter/material.dart";
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Coffee Card"),
+          backgroundColor: Colors.brown[700],
+        ),
+        body: Container(
+          color: Colors.orange,
+          width: 200,
+          height: 200,
+          margin: EdgeInsets.fromLTRB(10, 30, 0, 0),
+          child: Text("Coffee Card"),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:coffee_card/home.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Coffee Card"),
-          backgroundColor: Colors.brown[700],
-        ),
-        body: Container(
-          color: Colors.orange,
-          width: 200,
-          height: 200,
-          margin: EdgeInsets.fromLTRB(10, 30, 0, 0),
-          child: Text("Hello Emeka"),
-        ),
-      ),
-    );
-  }
+  runApp(Home());
 }
