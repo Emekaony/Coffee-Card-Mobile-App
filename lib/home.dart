f4d69411009a0512b8dbf58d62d1f8e737d1a2d1
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-03-04 22:32:51 -0800

    Did some refactoring

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:coffee_card/coffee_preps.dart";
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -8,15 +9,16 @@ class Home extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Coffee Card"),
+          title: Text(
+            "Coffee Card",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
           backgroundColor: Colors.brown[700],
         ),
         body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              alignment: Alignment.center,
               margin: EdgeInsets.only(bottom: 10.0),
               color: Colors.brown[200],
               padding: const EdgeInsets.all(20),
@@ -27,14 +29,7 @@ class Home extends StatelessWidget {
               margin: EdgeInsets.only(bottom: 10.0),
               color: Colors.brown[200],
               padding: const EdgeInsets.all(20),
-              child: const Text("How I like my coffee..."),
-            ),
-            Container(
-              alignment: Alignment.center,
-              margin: EdgeInsets.only(bottom: 10.0),
-              color: Colors.brown[200],
-              padding: const EdgeInsets.all(20),
-              child: const Text("How I like my coffee..."),
+              child: CoffeePreps(),
             ),
           ],
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,3 @@ import 'package:coffee_card/home.dart';
 void main() {
   runApp(Home());
 }
-
-class SandBox extends StatelessWidget {
-  const SandBox({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("SandBox"),
-          backgroundColor: Colors.grey,
-        ),
-        body: Text("emeka"),
-      ),
-    );
-  }
-}
