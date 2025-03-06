8f853c142a94e53120e5b1744d971c453c5189f6
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-03-06 03:37:27 -0800

    Added assets

---
+++ lib/coffee_preps.dart
@@ -0,0 +1,32 @@
+import "package:flutter/material.dart";
+
+class CoffeePreps extends StatelessWidget {
+  const CoffeePreps({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Text("Strength: "),
+            Text("3"),
+            Image.asset(
+              "assets/images/coffee_bean.png",
+              width: 25,
+              color: Colors.brown[200],
+              colorBlendMode: BlendMode.multiply,
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Text("Sugars: "),
+            Text("3"),
+            Image.asset("assets/images/sugar_cube.png", width: 25),
+          ],
+        ),
+      ],
+    );
+  }
+}
